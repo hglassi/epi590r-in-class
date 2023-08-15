7e653d44463e2a4cd6cde93f684f1eb070e50714
Hannah Glassie <hglassi@emory,edu>
2023-08-15 14:58:30 -0400

    created functions

---
+++ functions.R
@@ -0,0 +1,21 @@
+x <- 3
+x^2
+
+square <- function()
+square(x)
+square(53)
+x^2
+square <- function() {x^2}
+square(x=2)
+square <- function(x) {x^2}
+square(2)
+raise <- function (x,r) {x^r}
+raise(2, 2)
+raise (x=3, r=3)
+raise2 <- function(x, power) {x^power}
+raise <- function(x, r= 2) {x^r}
+raise (2, 3)
+raise(2)
+raise(6,3)
+raise(6)
+raise (1)
